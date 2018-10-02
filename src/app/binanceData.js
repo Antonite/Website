642f1a0f868e3364c62e68595e4e1ff5b202c4fe
antonite <axp9304@rit.edu>
2018-10-02 14:25:15 -0400

    fix image

--- src/app/binanceData.js
+++
@@ -1,6 +0,0 @@
-const binance = require('node-binance-api');
-binance.options({
-  'APIKEY': 'lJROQ4Orkt5wfy4lPHimV0mkv9QoDTly6QlnEFwgqXSYM7lXsgWpZSp0yi03FPFz',
-  'APISECRET': 'xC8uRpNFXIW6GWNVUX0BdJ4aGoXzVBSx2W872Zpldt0jfItAIj75JJfSZvYLtY5L'
-});
-

35492af8486aeffe9369cf9d74123be624ba1d17
antonite <axp9304@rit.edu>
2018-10-23 21:00:03 -0400

    added highcharts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { WorkExperiences, Education } from './experience/experience-data';
-import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -15,16 +14,6 @@ export class AppComponent {
 
   public ngOnInit()
   {
-    // $("#toolsTitle").hide();
-    // $("#languageTitle").hide();
 
-    // $("#manlanguage").hover(function(){
-    //   $("#toolsTitle").show();
-    //   $("#languageTitle").show();
-    // },
-    // function() {
-    //   $("#toolsTitle").hide();
-    //   $("#languageTitle").hide();
-    // });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 import { AppComponent } from './app.component';
 import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExperienceComponent
+    ExperienceComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]

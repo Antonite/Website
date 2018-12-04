07d3d6416341e3baba49ac9366ed80d48c4c4981
antonite <axp9304@rit.edu>
2018-12-03 20:30:53 -0500

    add caisson to experience

--- src/app/experience/experience-data.ts
+++ src/app/experience/experience-data.ts
@@ -2,17 +2,24 @@ import { Experience } from './experience'
 
 export const WorkExperiences: Experience[] = [
     {
+        name: "Caisson",
+        date: "December 2018 - Current",
+        position: "Software Developer",
+        description: "Developing a system that allows for individuals to take ownership of facts about their identity established by a third party, and share these facts in a verifiable way with interested parties.",
+        location: "New York, NY"
+    },
+    {
         name: "Citigroup",
-        date: "September 2016 - Current",
+        date: "September 2016 - November 2018",
         position: "Assistant Vice President",
-        description: "Front end C#/WPF development of PRISM - Citi Equities Risk Management Platform. The project's goal is to utilize the latest of .NET to create a modern and performant risk management platform for Citi's front office.",
+        description: "Front end development of PRISM - Citi Equities Risk Management Platform. The project's goal is to utilize the latest of .NET to create a modern and performant risk management platform for Citi's front office.",
         location: "Jersey City, NJ"
     },
     {
         name: "Citigroup",
         date: "June 2015 - September 2016",
         position: "Tech Analyst",
-        description: "Front end C#/WPF development of Citi Velocity - the industry's leading FX trading platform. Velocity serves thousands of internal and external Citi clients. The position also required full stack development of internal management tools.",
+        description: "Front end development of Citi Velocity - the industry's leading FX trading platform. Velocity, based on a .NET stack, serves thousands of internal and external Citi clients. The position also required full stack development of internal management tools.",
         location: "New York, NY"
     },
     {
